77aad9d37fcc3d2ad10ac072ee0697d706cbb101
t-i-0414 <takuya.iwashiro@takudev.net>
2025-07-09 03:11:49 +0900

    chore: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-empty': [0],
+    'scope-enum': [2, 'always', ['backend', 'mobile', 'admin', 'eslint-configs', 'eslint-rules']],
+  },
 };
