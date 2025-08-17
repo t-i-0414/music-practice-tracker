025dfdf2dc617002a6f112885343c44100f4e686
Takuya Iwashiro <takuya.iwashiro@takudev.net>
2025-08-18 00:43:10 +0900

    feat(backend): add AppAuthGuard

---
+++ packages/apps/backend/src/guards/app-auth-guard/strategy.ts
@@ -0,0 +1,49 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { UserStatusRecord } from '@/modules/aggregate/user/user.constants';
+import { UserQueryService } from '@/modules/aggregate/user/user.query.service';
+
+export type Payload = {
+  sub: string;
+  iat?: number;
+  exp?: number;
+};
+
+@Injectable()
+export class AppPassportStrategy extends PassportStrategy(Strategy, 'app') {
+  public constructor(
+    private readonly configService: ConfigService,
+    private readonly userQuery: UserQueryService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+    });
+  }
+
+  public async validate(payload: Payload): Promise<{ publicId: string; email: string; name: string }> {
+    try {
+      const user = await this.userQuery.findUserByIdOrFail({ publicId: payload.sub });
+
+      if (user.status !== UserStatusRecord.ACTIVE) {
+        throw new UnauthorizedException('Account is not active');
+      }
+
+      return {
+        publicId: user.publicId,
+        email: user.email,
+        name: user.name,
+      };
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw error;
+      }
+
+      throw new UnauthorizedException('User not found or invalid token');
+    }
+  }
+}
