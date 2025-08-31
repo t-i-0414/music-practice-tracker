95d56453ac5bfbdd45ba70639e56127daee008e9
Takuya Iwashiro <takuya.iwashiro@takudev.net>
2025-08-31 22:51:16 +0900

    refactor: remove unused files

--- packages/apps/backend/src/apis/admin/module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { AdminAdminUsersModule } from './admin-users/module';
-import { AdminUsersModule } from './users/module';
-
-@Module({
-  imports: [AdminUsersModule, AdminAdminUsersModule],
-})
-export class AdminApiModule {}
--- packages/apps/backend/src/decorators/api-controller/decorator.ts
+++
@@ -1,5 +0,0 @@
-import { Controller, applyDecorators } from '@nestjs/common';
-
-export function ApiController(path: string): ClassDecorator {
-  return applyDecorators(Controller(`api/${path}`));
-}
