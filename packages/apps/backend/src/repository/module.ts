401e4b10dd95dd2c7a0d1e38a46a028c5ff51188
Takuya Iwashiro <takuya.iwashiro@takudev.net>
2025-09-01 15:21:32 +0900

    refactor: remove unused files

--- packages/apps/backend/src/repository/module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { RepositoryService } from './service';
-
-@Module({
-  providers: [RepositoryService],
-  exports: [RepositoryService],
-})
-export class RepositoryModule {}
