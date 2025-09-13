d027b0df457c1f443e3de7cf01dce121ee6f43c6
Takuya Iwashiro <takuya.iwashiro@takudev.net>
2025-09-13 17:58:09 +0900

    feat: update migration files

--- packages/apps/backend/prisma/migrations/20250727223832_create_users_table/migration.sql
+++ packages/apps/backend/prisma/migrations/20250727223832_create_users_table/migration.sql
@@ -1,10 +1,10 @@
-CREATE TYPE "public"."user_status" AS ENUM ('ACTIVE', 'INACTIVE', 'SUSPENDED', 'PENDING', 'BANNED');
+CREATE TYPE "public"."user_status" AS ENUM ('ACTIVE', 'PENDING', 'SUSPENDED', 'BANNED');
 
 CREATE TABLE
   "users" (
     "id" SERIAL NOT NULL,
     "public_id" UUID NOT NULL DEFAULT gen_random_uuid (),
-    "email" TEXT NOT NULL,
+    "firebase_uid" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "status" "public"."user_status" NOT NULL DEFAULT 'PENDING',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -13,7 +13,7 @@ CREATE TABLE
   );
 
 CREATE UNIQUE INDEX "users_public_id_key" ON "users" ("public_id");
-CREATE UNIQUE INDEX "users_email_key" ON "users" ("email");
-CREATE INDEX "users_name_idx" ON "users" ("name");
+CREATE UNIQUE INDEX "users_firebase_uid_key" ON "users" ("firebase_uid");
+CREATE UNIQUE INDEX "users_name_idx" ON "users" ("name");
 CREATE INDEX "users_status_idx" ON "users" ("status");
 CREATE INDEX "users_created_at_idx" ON "users" ("created_at");
