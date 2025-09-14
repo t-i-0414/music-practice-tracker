cb8a25edda6a945dd49c94643bc93630155925bd
Takuya Iwashiro <takuya.iwashiro@takudev.net>
2025-09-15 01:54:09 +0900

    chore: update AppApiUsersModule

--- packages/apps/backend/src/apis/app/auth/auth.module.ts
+++ packages/apps/backend/src/apis/app/auth/auth.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppApiAuthController } from './auth.controller';
 
-import { FirebaseAuthModule } from '@/domain/aggregates/firebase-auth/firebase-auth.module';
 import { UserModule } from '@/domain/aggregates/user/user.module';
 import { UserAuthModule } from '@/domain/usecases/user-auth/user-auth.module';
 
 @Module({
-  imports: [FirebaseAuthModule, UserModule, UserAuthModule],
+  imports: [UserModule, UserAuthModule],
   controllers: [AppApiAuthController],
 })
 export class AppApiAuthModule {}
