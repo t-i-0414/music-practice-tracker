a00d403b707f0db6df5a16ec1da44a833c7a3704
Takuya Iwashiro <takuya.iwashiro@takudev.net>
2025-09-15 18:47:20 +0900

    feat: update user auth module

--- packages/apps/backend/src/domain/usecases/user-auth/user-auth.module.ts
+++ packages/apps/backend/src/domain/usecases/user-auth/user-auth.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 
-import { UserAuthService } from './user-auth.service';
+import { CreateUserService } from './create-user.service';
+import { DeleteUserService } from './delete-user.service';
+import { VerifyFirebaseIdTokenService } from './verify-firebase-id-token.service';
 
 import { FirebaseAuthModule } from '@/domain/aggregates/firebase-auth/firebase-auth.module';
 import { UserModule } from '@/domain/aggregates/user/user.module';
 
 @Module({
   imports: [FirebaseAuthModule, UserModule],
-  providers: [UserAuthService],
-  exports: [UserAuthService],
+  providers: [DeleteUserService, VerifyFirebaseIdTokenService, CreateUserService],
+  exports: [DeleteUserService, VerifyFirebaseIdTokenService, CreateUserService],
 })
 export class UserAuthModule {}
