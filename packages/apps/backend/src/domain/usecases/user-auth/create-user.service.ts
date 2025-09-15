91b22a74c0e5cd189d7be59f43cc59fb0a07e117
Takuya Iwashiro <takuya.iwashiro@takudev.net>
2025-09-16 00:32:16 +0900

    refactor: remove VerifyFirebaseIdTokenService

--- packages/apps/backend/src/domain/usecases/user-auth/create-user.service.ts
+++ packages/apps/backend/src/domain/usecases/user-auth/create-user.service.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { validateSync } from 'class-validator';
 
-import { VerifyFirebaseIdTokenService } from './verify-firebase-id-token.service';
-
 import { ApiError } from '@/apis/utils/api.error';
+import { FirebaseAuthService } from '@/domain/aggregates/firebase-auth/firebase-auth.service';
 import { isProviderAllowed } from '@/domain/aggregates/firebase-auth/utils/constants';
 import { UserCommandService } from '@/domain/aggregates/user/user.command.service';
 import { UserQueryService } from '@/domain/aggregates/user/user.query.service';
@@ -14,7 +13,7 @@ import { NON_ERROR_LENGTH, transformValidationErrorIntoDetail } from '@/utils/tr
 @Injectable()
 export class CreateUserService {
   public constructor(
-    private readonly verifyFirebaseIdTokenService: VerifyFirebaseIdTokenService,
+    private readonly firebaseAuthService: FirebaseAuthService,
     private readonly usersQueryService: UserQueryService,
     private readonly usersCommandService: UserCommandService,
   ) {}
@@ -24,7 +23,7 @@ export class CreateUserService {
       uid,
       email_verified: emailVerified,
       firebase: { sign_in_provider: signInProvider },
-    } = await this.verifyFirebaseIdTokenService.execute(idToken);
+    } = await this.firebaseAuthService.verifyIdToken(idToken);
 
     if (!(emailVerified === true || isProviderAllowed(signInProvider))) {
       throw new ApiError('AP0403', 'Email verification required');
