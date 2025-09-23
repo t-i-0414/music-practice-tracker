90ef535dadd9f5077387a623ca40a04b30d31d29
Takuya Iwashiro <takuya.iwashiro@takudev.net>
2025-09-24 01:08:04 +0900

    test: update config

--- packages/apps/backend/tests/jest-env.setup.ts
+++ packages/apps/backend/tests/jest-env.setup.ts
@@ -2,5 +2,5 @@ import { resolve } from 'node:path';
 
 import { config as loadEnv } from 'dotenv';
 
-loadEnv({ path: resolve(resolve(__dirname, '..'), '.env.test'), override: true });
+loadEnv({ path: resolve(resolve(__dirname, '..'), '.env.test'), override: true, quiet: true });
 process.env.NODE_ENV = 'test';
